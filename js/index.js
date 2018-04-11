32468d64470ac50c9b461cb3ae938ad74f5c48e2
jusola <juusolain@gmail.com>
2018-04-11 19:27:43 +0300

    First working version

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var dictionary = "";
+
+function StartGen(){
+  $.getJSON( "dictionary/words.json", function( json ) {
+    dictionary = json;
+    SelectWords();
+ });
+}
+
+
+function getRandomInt(min, max) {
+    var byteArray = new Uint32Array(1);
+    window.crypto.getRandomValues(byteArray);
+
+    var range = max - min + 1;
+    var max_range = 4294967295;
+    if (byteArray[0] >= Math.floor(max_range / range) * range)
+        return getRandomInt(min, max);
+    return min + (byteArray[0] % range);
+}
+
+
+function SelectWords(){
+  var password = "";
+  for (var i = 0; i < 2; i++){
+    var tempWord = dictionary.array[getRandomInt(0, 274917)];
+    tempWord = tempWord.charAt(0).toUpperCase() + tempWord.slice(1);
+    password+=tempWord;
+  }
+  if(password!=""){
+    document.getElementById("password").innerHTML = password;
+    console.log(password);
+  }else{
+    console.log("password generation failed");
+  }
+}
